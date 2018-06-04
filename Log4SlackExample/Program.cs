9e98dc37dcb220eb956b74cfd5cd7cd1168ef6bc
Neo Ho <neo.ho@johnsontechinc.com>
2018-06-04 20:08:25 +0800

    [feat] Log4Slack POC.

---
+++ Log4SlackExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using log4net;
+using log4net.Config;
+
+namespace Log4SlackExample
+{
+    internal class Program
+    {
+        private static readonly Lazy<ILog> LazyLog = new Lazy<ILog>(() =>
+        {
+            var log = LogManager.GetLogger(typeof(Program));
+            XmlConfigurator.Configure();
+            return log;
+        });
+
+        private static ILog Logger => LazyLog.Value;
+
+        private static void Main()
+        {
+            Console.WriteLine("Enter the message you want to send to slack...");
+            var msg = Console.ReadLine();
+            Logger.Info(msg);
+
+            Console.Read();
+        }
+    }
+}
